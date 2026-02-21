8fce3cc2286875913537eac1f8c85b1067a690a3
Brian Padgett <SDETBMan-brian.padgett11@outlook.com>
2026-02-21 11:57:41 -0700

    Fix CartPage compilation error: add missing WebElement and ExpectedConditions imports
    clickCheckout() was updated to use direct JS click but the required
    WebElement and ExpectedConditions imports were not added, causing a
    compilation failure. Adds both imports to resolve the build error.

--- selenium-java/src/main/java/com/framework/pages/CartPage.java
+++ selenium-java/src/main/java/com/framework/pages/CartPage.java
@@ -6,6 +6,8 @@ import io.appium.java_client.ios.IOSDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class CartPage extends BasePage {
 
